758abd4d865101a19a37b4686e5a831e2e32dca8
Zauri Khutsishvili <106984285+zauri-khutsishvili@users.noreply.github.com>
2023-03-21 22:41:57 +0400

    Update JS

--- js/script.js
+++ js/script.js
@@ -3,14 +3,18 @@ const thankYou = document.querySelector(".thank-you");
 const rateSubmit = document.getElementById("rate-submit");
 const rating = document.getElementById("rating");
 const points = document.querySelectorAll(".rate__point");
-
-rateSubmit.addEventListener("click", () => {
-    thankYou.classList.remove("hidden");
-    rate.classList.add("hidden");
-})
+let active;
 
 points.forEach((point) => {
     point.addEventListener("click", () => {
         rating.innerText = point.innerText;
+        active = true;
     })
+})
+
+rateSubmit.addEventListener("click", () => {
+    if (active) {
+        thankYou.classList.remove("hidden");
+        rate.classList.add("hidden");
+    }
 })
